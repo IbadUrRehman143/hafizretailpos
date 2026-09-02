470687e4af37f5914f6e6d2b1cd66b5276878bec
IbadUrRehman143 <ibadurrehman010@gmail.com>
2026-09-02 04:25:17 +0300

    ok

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   devIndicators: false,
-} as any;
+};
 
 export default nextConfig;
